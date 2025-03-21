59bfa113e192209f7acfa5be804b16b157d1a800
Edison Leung <eleung41@myseneca.ca>
2025-03-21 14:05:25 -0400

    completed lab7a.py

--- lab7a.py
+++ lab7a.py
@@ -2,10 +2,10 @@
 # Student ID: [seneca_id]
 class Time:
     """Simple object type for time of the day.
-    data attributes: hour, minute, second
+       data attributes: hour, minute, second
     """
-    def __init__(self,hour=12,minute=0,second=0):
-        """constructor for time object""" 
+    def __init__(self, hour=12, minute=0, second=0):
+        """constructor for time object"""
         self.hour = hour
         self.minute = minute
         self.second = second
@@ -15,11 +15,21 @@ def format_time(t):
     return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}'
 
 def sum_times(t1, t2):
-    """Add two time objests and return the sum."""
-    sum = Time(0,0,0)
+    """Add two time objects and return the sum."""
+    sum = Time(0, 0, 0)
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+
+    if sum.second >= 60:
+        sum.minute += sum.second // 60
+        sum.second %= 60
+    if sum.minute >= 60:
+        sum.hour += sum.minute // 60
+        sum.minute %= 60
+    if sum.hour >= 24:
+        sum.hour %= 24
+
     return sum
 
 def valid_time(t):
